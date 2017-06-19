451e1323b61c9a9ed385c0b57f5f91408eabfbd2
avinashsingh1 <avinash.singh@perficient.com>
2017-06-19 16:29:46 +0530

    Display, drag and drop shapes has been added.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 canvas = [];
+
 //Populating JSON Data
 $.getJSON('https://jsonplaceholder.typicode.com/albums', function (data) {
 	var item = [];
@@ -10,7 +11,7 @@ $.getJSON('https://jsonplaceholder.typicode.com/albums', function (data) {
 		}
 	});
 });
-
+			
 //Canvas and Shape Generation
 $(document).ready(function(){
 	$(".generateCanvas").on('click',function(){   
@@ -24,7 +25,8 @@ $(document).ready(function(){
 		canvas[i]  = new fabric.Canvas('myCanvas'+i);
 	   }
 	});
-	
+   
+   
 	// Adding Shapes
 	$(".insertShape").click(function(){
 		var selectedCanvas = $("#canvasList option:selected").val();
@@ -41,17 +43,16 @@ $(document).ready(function(){
 				break;
 		}
 		canvasPlacement();
-	});
+	}); 
 });
-
 
 // Defining Shapes
 function drawCircle(canvasId){
 	var canvas1 = canvas[canvasId];
 	var object1 = new fabric.Circle({
 		radius: 30,
-		fill: 'black',
-		left: 70,
+		fill: 'red',
+		left: 100,
 		top: 10
 	});
 	canvas1.add(object1);
@@ -63,7 +64,7 @@ function drawRect(canvasId){
 	var object2 = new fabric.Rect({
 		width: 80,
 		height: 50,
-		fill: 'black',
+		fill: 'green',
 		opacity: 1,
 		left: 10,
 		top: 10
@@ -76,10 +77,52 @@ function drawSqr(canvasId){
 	var object2 = new fabric.Rect({
 		width: 50,
 		height: 50,
-		fill: 'black',
+		fill: 'blue',
 		opacity: 1,
-		left: 10,
-		top: 10
+		left: 80,
+		top: 80
 	});
 	canvas1.add(object2);
 }
+
+//Canvas Selection
+function canvasPlacement(){
+	var canvasLength = $("#counter").val();
+	for(var i = 0; i < canvasLength; i++){
+		var canvasSelect = canvas[i];
+		canvasSelect.on('mouse:down', function() {
+			if(this.getActiveObject()) {
+				activeObject  = $.extend({}, this.getActiveObject());
+				initialCanvas = this.lowerCanvasEl.id;
+			}
+		});
+	}
+}
+
+//Canvas Object Move
+$(document).on('mouseup', function(evt) {
+	if(evt.target.localName === 'canvas' && initialCanvas) {
+		canvasId = $(evt.target).siblings().attr('id');
+		var canvas1 = parseInt(initialCanvas.replace('myCanvas',''));
+		canvas1 = canvas[canvas1];
+		
+		var canvas2 = parseInt(canvasId.replace('myCanvas',''));
+		canvas2 = canvas[canvas2];
+		console.log(canvas1);
+		console.log(canvas2);
+		var posX = evt.clientX;
+		var posY = evt.clientY;
+		canvasWidth = canvas1.width;
+		objectHeight = activeObject.height;
+		objectHeight = objectHeight/2;
+		objectWidth = activeObject.width;
+		objectWidth = objectWidth/2;
+		if(canvasId !== initialCanvas) {
+			canvas2.add(activeObject);
+			canvas2.renderAll();
+			canvas1.getActiveObject().remove();
+		}
+	}
+	initialCanvas = '';
+	activeObject  = {};                 
+});
