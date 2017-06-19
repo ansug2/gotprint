db246f2e6fed998345c42ee3c9a46f73f4aa8cd1
avinashsingh1 <avinash.singh@perficient.com>
2017-06-19 15:44:43 +0530

    Appending canvas and list of canvas.

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ $.getJSON('https://jsonplaceholder.typicode.com/albums', function (data) {
 		}
 	});
 });
+
+//Canvas and Shape Generation
+$(document).ready(function(){
+	$(".generateCanvas").on('click',function(){   
+	   $(".canvasContainer").html('');
+	   $("#canvasList option").remove();
+	   $("<option value='0'>Select a Canvas to Draw</option>").appendTo("#canvasList");
+	   var canvasCounter = $("#counter").val();
+	   for(var i = 0; i < canvasCounter; i++){
+		$("<canvas id='myCanvas"+i+"' width='200' height='200'></canvas>").appendTo(".canvasContainer");
+		$("<option value="+i+">Canvas "+i+"</option>").appendTo("#canvasList");
+	   }
+	});
+});
