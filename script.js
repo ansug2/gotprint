799c53ba059df6ffb0586d1c99e27cb92e94851f
avinashsingh1 <avinash.singh@perficient.com>
2017-06-19 16:11:00 +0530

    Added shapes and events to display it on canvas.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+canvas = [];
 //Populating JSON Data
 $.getJSON('https://jsonplaceholder.typicode.com/albums', function (data) {
 	var item = [];
@@ -20,6 +21,65 @@ $(document).ready(function(){
 	   for(var i = 0; i < canvasCounter; i++){
 		$("<canvas id='myCanvas"+i+"' width='200' height='200'></canvas>").appendTo(".canvasContainer");
 		$("<option value="+i+">Canvas "+i+"</option>").appendTo("#canvasList");
+		canvas[i]  = new fabric.Canvas('myCanvas'+i);
 	   }
 	});
+	
+	// Adding Shapes
+	$(".insertShape").click(function(){
+		var selectedCanvas = $("#canvasList option:selected").val();
+		var selectedShape = parseInt($("#shapeList option:selected").val());
+		switch (selectedShape){
+			case 1: 
+				drawCircle(selectedCanvas);
+				break;
+			case 2: 
+				drawSqr(selectedCanvas);
+				break;
+			case 3:
+				drawRect(selectedCanvas);
+				break;
+		}
+		canvasPlacement();
+	});
 });
+
+
+// Defining Shapes
+function drawCircle(canvasId){
+	var canvas1 = canvas[canvasId];
+	var object1 = new fabric.Circle({
+		radius: 30,
+		fill: 'black',
+		left: 70,
+		top: 10
+	});
+	canvas1.add(object1);
+	
+}
+
+function drawRect(canvasId){
+	var canvas1 = canvas[canvasId]; 
+	var object2 = new fabric.Rect({
+		width: 80,
+		height: 50,
+		fill: 'black',
+		opacity: 1,
+		left: 10,
+		top: 10
+	});
+	canvas1.add(object2);
+}
+
+function drawSqr(canvasId){
+	var canvas1 = canvas[canvasId]; 
+	var object2 = new fabric.Rect({
+		width: 50,
+		height: 50,
+		fill: 'black',
+		opacity: 1,
+		left: 10,
+		top: 10
+	});
+	canvas1.add(object2);
+}
